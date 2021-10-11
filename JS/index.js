00fd55a77e7b08fef3e680d96f3ea1d5eeb07174
Jasika16 <52914913+Jasika16@users.noreply.github.com>
2021-10-11 14:02:36 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+ 
+
+
+burger.addEventListener('click', ()=>{
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+    
+})
+var today = new Date();
+
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+var dateTime = date+' '+time;
+
+console.log(dateTime);
+
+document.getElementById("date-time").innerHTML = dateTime;
